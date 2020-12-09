c8706e4db9b56c6511e1c484aac78008b78b8058
Víctor Piñón <victorpinonrattia@hotmail.com>
2020-12-09 21:24:29 +0100

    ex 3.2

--- index.js
+++ index.js
@@ -25,15 +25,20 @@ let persons = [
     }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
 })
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
+app.get('/info', (request, response) => {
+    const numberOfPeople = persons.length
+    const now = new Date()
+    response.send(
+        `<p>Phonebook has info for ${numberOfPeople} people</p>
+         <p>${now}</p>`
+    )
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
