70169dd2f634fb730c05810920d85ad7814d824d
Víctor Piñón <victorpinonrattia@hotmail.com>
2020-12-10 20:19:54 +0100

    ex 3.8

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import morgan from "morgan"
 
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (req, res) =>  Object.keys(req.body).length > 0 ? JSON.stringify(req.body) : "" )
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
