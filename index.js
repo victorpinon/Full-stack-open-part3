eb5d55680538a4f6f1da9c7689caf81346f0764c
Víctor Piñón <victorpinonrattia@hotmail.com>
2020-12-12 10:28:13 +0100

    added frontend

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from "cors"
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('body', (req, res) =>  Object.keys(req.body).length > 0 ? JSON.stringify(req.body) : "" )
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
