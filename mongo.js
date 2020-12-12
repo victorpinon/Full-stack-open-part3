5e95467b074c14889431d52f7fe664667f662115
Víctor Piñón <victorpinonrattia@hotmail.com>
2020-12-12 19:39:19 +0100

    ex 3.12

---
+++ mongo.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const generateId = () => (Math.floor(Math.random() * 1000000))
+
+const url =
+    `mongodb+srv://victorpinon:${password}@cluster0.09dp7.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 5) {
+    const person = new Person({
+        id: generateId(),
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+    
+    person.save().then(result => {
+        console.log(`added ${result.name} number ${result.number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+else if (process.argv.length === 3) {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person)
+        })
+        mongoose.connection.close()
+    })
+}
+
