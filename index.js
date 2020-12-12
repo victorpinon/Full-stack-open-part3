4439da95bc266c6000caf21fea36aac0c97ac1ae
Víctor Piñón <victorpinonrattia@hotmail.com>
2020-12-12 22:33:05 +0100

    ex 3.18

--- index.js
+++ index.js
@@ -13,29 +13,6 @@ app.use(express.static('build'))
 morgan.token('body', (req, res) =>  Object.keys(req.body).length > 0 ? JSON.stringify(req.body) : "" )
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-        name: "Arto Hellas",
-        number: "040-123456",
-        id: 1
-    },
-    {
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-        id: 2
-    },
-    {
-        name: "Dan Abramov",
-        number: "12-43-234345",
-        id: 3
-    },
-    {
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-        id: 4
-    }
-]
-
 const generateId = () => (Math.floor(Math.random() * 1000000))
 
 app.get('/api/persons', (request, response, next) => {
@@ -45,26 +22,24 @@ app.get('/api/persons', (request, response, next) => {
     .catch(error => next(error))
 })
 
-// TO-DO
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    
-    if (person) {
+    Person.findById(request.params.id).then(person => {
         response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    })
+    .catch(error => next(error))
 })
 
-//TO-DO
 app.get('/info', (request, response) => {
-    const numberOfPeople = persons.length
-    const now = new Date()
-    response.send(
-        `<p>Phonebook has info for ${numberOfPeople} people</p>
-         <p>${now}</p>`
-    )
+    Person.find({}).then(people => {
+        const numberOfPeople = people.length
+        const now = new Date()
+        response.send(
+            `<p>Phonebook has info for ${numberOfPeople} people</p>
+            <p>${now}</p>`
+        )
+    })
+    .catch(error => next(error))
+    
 })
 
 app.post('/api/persons', (request, response, next) => {
@@ -78,11 +53,6 @@ app.post('/api/persons', (request, response, next) => {
     else if (!body.number) {
         return response.status(400).json({ 
             error: 'number missing' 
-        })
-    }
-    else if (persons.find(p => p.name === body.name)) {
-        return response.status(409).json({ 
-            error: 'name must be unique' 
         })
     }
 
